6cf1e32cde3e57debe72ae1705e83aa97d92170d
ojimcy <ojimcy247@gmail.com>
2022-06-02 11:18:53 +0100

    NFT marketplace

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/marketplace', (req, res) => {
-    res.render("marketplace", {layout: 'dashboard'})
+    res.render("marketplace", {layout: 'marketplace'})
 })
 
 
